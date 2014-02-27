8a970f512130d13c2da2d3abf339f0d0675588c0
torres <torres@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-02-27 17:53:35 +0000

    0.0.2
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/xkcd@31 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/theme_xkcd.r
+++ pkg/R/theme_xkcd.r
@@ -1,13 +1,13 @@
 # Emilio Torres Manzanera
 # University of Oviedo
-# Time-stamp: <2013-05-14 Tue 21:20 emilio on emilio-laptop2>
+# Time-stamp: <2014-02-27 jue 16:10 emilio on emilio-Satellite-P100>
 # =====================================================================
 
 ## fibosworld 2013. Change fonts in ggplot2, and create xkcd style graphs \url{http://fibosworld.wordpress.com/2013/02/17/change-fonts-in-ggplot2-and-create-xkcd-style-graphs/}
 
 theme_xkcd <- function(){
-  require(extrafont)
-  if( "xkcd" %in% fonts() ) {
+  ##require(sysfonts)
+  if( "xkcd" %in% font.families() ) {
   theme(panel.grid.major = element_blank(),
         ##axis.ticks = element_blank(),
         axis.ticks = element_line(colour = "black"),
@@ -17,7 +17,7 @@ theme_xkcd <- function(){
         strip.background = element_blank(),
         text = element_text(size = 16, family = "xkcd"))
   } else {
-    warning("Not xkcd fonts installed!")
+    warning("Not xkcd fonts installed! See the vignette")
   theme(panel.grid.major = element_blank(),
         ##axis.ticks = element_blank(),
         axis.ticks = element_line(colour = "black"),
